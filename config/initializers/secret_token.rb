5c9b0d6779eb1ae7fcfba50f1f9feb800246e69b
Jarmo Isotalo <jamo@isotalo.fi>
2012-10-26 00:44:00 +0300

    vanilla rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailerTest::Application.config.secret_token = 'b3be0505f1988e93e24db77440f9e6e7e079509c047d4d55837b55daa1ce4655a77a2fe599db8e45a7e3c38053df64cd350d3d8ca47768c5d4760395f3721eec'
